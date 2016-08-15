68a471068185924c0572ddea471710c68b685b1a
Joshua Mabina <mabinajoshua@gmail.com>
2016-08-15 18:11:30 +0300

    Implenting the dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+        <div class="col-md-7 col-md-offset-1">
+			<div class="page-header">
+				<h1>Dashboard</h1>
+			</div>
+			<div class="well">
+				<div class="">
 
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+				</div>
+
+			</div>
+		</div>
     </div>
 </div>
 @endsection
