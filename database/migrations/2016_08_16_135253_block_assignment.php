80c2895968659414498d057d9f56f6b5a7a4283f
hatibumungia <hatibumungia@gmail.com>
2016-08-18 19:10:02 +0300

    code cleaning

--- database/migrations/2016_08_16_135253_block_assignment.php
+++ database/migrations/2016_08_16_135253_block_assignment.php
@@ -20,6 +20,7 @@ class BlockAssignment extends Migration
             $table->foreign('areas_type_id')->references('areas_type_id')->on('area_assignment')->onDelete('cascade');
 
             $table->integer('block_id')->unsigned();
+            $table->text('file_name')->nullable();
             $table->foreign('block_id')->references('block_id')->on('blocks')->onDelete('cascade');
 
             //making composite keys
