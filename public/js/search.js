abdac43a1aa4737b2f8bdf04e112f0ef517ac84b
= <=>
2016-08-21 15:48:23 +0300

    Change the labels of search table

--- public/js/search.js
+++ public/js/search.js
@@ -7,7 +7,7 @@ $(function () {
 
     function getSearchResults(params = ''){
 
-        $('#searchResults').html("<table id='example' class='display' cellspacing='0' width='100%'><thead><tr><th>Area</th><th>Type</th><th>Block</th><th>Plot#</th><th>Size</th><th>Price</th></tr></thead><tfoot><tr><th>Name</th><th>Position</th><th>Office</th><th>Extn.</th><th>Start date</th><th>Salary</th></tr></tfoot></table>");
+        $('#searchResults').html("<table id='example' class='display' cellspacing='0' width='100%'><thead><tr><th>Location</th><th>Land use</th><th>Block</th><th>Plot#</th><th>Size</th><th>Price</th></tr></thead><tfoot><tr><th>Location</th><th>Land use</th><th>Block</th><th>Plot#</th><th>Size</th><th>Price</th></tr></tfoot></table>");
 
         var url = '/search/performSearch' + params;
         $('#example').DataTable( {
