f827969c1e9ee2f3704562754edb838beee55a17
= <=>
2016-08-21 15:39:00 +0300

    Remove the bootstrap's carousel and thumbnails

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -6,34 +6,6 @@
 
 
 <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 hidden-xs">
-        <div id="carousel-299058" class="carousel slide">
-          <ol class="carousel-indicators">
-            <li data-target="#carousel-299058" data-slide-to="0" class=""> </li>
-            <li data-target="#carousel-299058" data-slide-to="1" class="active"> </li>
-            <li data-target="#carousel-299058" data-slide-to="2" class=""> </li>
-          </ol>
-          <div class="carousel-inner">
-            <div class="item">
-              <img class="img-responsive" src="/img/1920x500.gif" alt="thumb">
-              <!-- <img class="img-responsive" src="http://lorempixel.com/1920/500/city/8" alt="thumb"> -->
-              <div class="carousel-caption"> Carousel caption. Here goes slide description. Lorem ipsum dolor set amet. </div>
-            </div>
-            <div class="item active"> 
-              <img class="img-responsive" src="/img/1920x500.gif" alt="thumb">
-              <!-- <img class="img-responsive" src="http://lorempixel.com/1920/500/city/9" alt="thumb"> -->
-              <div class="carousel-caption"> Carousel caption 2. Here goes slide description. Lorem ipsum dolor set amet. </div>
-            </div>
-            <div class="item"> 
-              <img class="img-responsive" src="/img/1920x500.gif" alt="thumb">
-              <!-- <img class="img-responsive" src="http://lorempixel.com/1920/500/city/10" alt="thumb"> -->
-              <div class="carousel-caption"> Carousel caption 3. Here goes slide description. Lorem ipsum dolor set amet. </div>
-            </div>
-          </div>
-          <a class="left carousel-control" href="#carousel-299058" data-slide="prev"><span class="icon-prev"></span></a> <a class="right carousel-control" href="#carousel-299058" data-slide="next"><span class="icon-next"></span></a></div>
-      </div>
-    </div>
     <hr>
   <div class="row">
     <div class="col-lg-9 col-md-12">
