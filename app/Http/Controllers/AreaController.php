285d05fedb48f453a3f3c6e97128224ca947d9f3
= <=>
2016-08-21 18:59:10 +0300

    Create the AreaController

---
+++ app/Http/Controllers/AreaController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Http\Requests\CreateLocationRequest;
+
+use App\Area;
+
+class AreaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $locations = Area::all();
+
+        return view('locations.index', compact('locations'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('locations.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateLocationRequest $request)
+    {
+        Area::create($request->all());
+
+        flash()->success($request->input('name'). ' added successfully');
+
+        return redirect('locations/create');        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($area_id)
+    {
+        $location = Area::findOrFail($area_id);
+
+        return view('locations.show', compact('location'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($area_id)
+    {
+        $location = Area::findOrFail($area_id);
+
+        return view('locations.edit', compact('location'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(CreateLocationRequest $request, $area_id)
+    {
+        $location = Area::findOrFail($area_id);
+
+        $location->update($request->all());
+
+        flash()->success($location->name . ' updated successfully');
+
+        return redirect('locations'); 
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($area_id)
+    {
+        $location = Area::findOrFail($area_id);
+
+        $location->delete();
+
+        flash()->success($location->name . ' deleted successfully');
+
+        return redirect('locations'); 
+
+    }
+}
