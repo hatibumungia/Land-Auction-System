1eef5e9cf0ea9421479374e02dbc9f946e1c8836
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-09-10 15:44:01 +0300

    Add col-sm-12 to the rows

--- resources/views/reports/clients/index.blade.php
+++ resources/views/reports/clients/index.blade.php
@@ -11,88 +11,92 @@
 @section('content')
 
     <div class="row">
-        <ol class="breadcrumb">
-            <li>
-                <a href="{{ url('/') }}">CDA</a>
-            </li>
-            <li class="active">Clients</li>
-        </ol>
+        <div class="col-sm-12">
+            <ol class="breadcrumb">
+                <li>
+                    <a href="{{ url('/') }}">CDA</a>
+                </li>
+                <li class="active">Clients</li>
+            </ol>
+        </div>
     </div>
 
     <div class="row">
 
-        <div role="tabpanel">
-            <!-- Nav tabs -->
-            <ul class="nav nav-tabs" role="tablist">
-                <li role="presentation" class="active">
-                    <a href="#all" aria-controls="all" role="tab" data-toggle="tab">All</a>
-                </li>
-                <li role="presentation">
-                    <a href="#reserved" aria-controls="reserved" role="tab" data-toggle="tab">Reserved</a>
-                </li>
-                <li role="presentation">
-                    <a href="#unreserved" aria-controls="unreserved" role="tab" data-toggle="tab">Unreserved</a>
-                </li>
-            </ul>
+        <div class="col-sm-12">
+            <div role="tabpanel">
+                <!-- Nav tabs -->
+                <ul class="nav nav-tabs" role="tablist">
+                    <li role="presentation" class="active">
+                        <a href="#all" aria-controls="all" role="tab" data-toggle="tab">All</a>
+                    </li>
+                    <li role="presentation">
+                        <a href="#reserved" aria-controls="reserved" role="tab" data-toggle="tab">Reserved</a>
+                    </li>
+                    <li role="presentation">
+                        <a href="#unreserved" aria-controls="unreserved" role="tab" data-toggle="tab">Unreserved</a>
+                    </li>
+                </ul>
 
-            <!-- Tab panes -->
-            <div class="tab-content">
-                <div role="tabpanel" class="tab-pane active" id="all">
+                <!-- Tab panes -->
+                <div class="tab-content">
+                    <div role="tabpanel" class="tab-pane active" id="all">
 
-                    @if(count($clients) > 0)
+                        @if(count($clients) > 0)
 
-                        <div class="table-responsive">
-                            <table class="table table-hover">
-                                <thead>
-                                <tr>
-                                    <th>First Name</th>
-                                    <th>Middle Name</th>
-                                    <th>Last Name</th>
-                                    <th>Email Address</th>
-                                    <th>Phone Number</th>
-                                    <th>Registered</th>
-                                    <th>Last Login</th>
-                                    <th>Region</th>
-                                    <th><i class="fa fa-info-circle"></i></th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($clients as $client)
+                            <div class="table-responsive">
+                                <table class="table table-hover">
+                                    <thead>
                                     <tr>
-                                        <td>{{ $client->first_name }}</td>
-                                        <td>{{ $client->middle_name }}</td>
-                                        <td>{{ $client->last_name }} </td>
-                                        <td>{{ $client->email_address }}</td>
-                                        <td>{{ $client->phone_number }}</td>
-                                        <td>{{ $client->created_at }}</td>
-                                        <td>{{ $client->updated_at }}</td>
-                                        <td>{{ $client->region }}</td>
-                                        <td>
-                                            <a href="{{ url('reports/clients/' . $client->user_detail_id) }}"
-                                               data-toggle="tooltip"
-                                               title="Click here for more details"><i class="fa fa-binoculars"></i></a>
-                                        </td>
+                                        <th>First Name</th>
+                                        <th>Middle Name</th>
+                                        <th>Last Name</th>
+                                        <th>Email Address</th>
+                                        <th>Phone Number</th>
+                                        <th>Registered</th>
+                                        <th>Last Login</th>
+                                        <th>Region</th>
+                                        <th><i class="fa fa-info-circle"></i></th>
                                     </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($clients as $client)
+                                        <tr>
+                                            <td>{{ $client->first_name }}</td>
+                                            <td>{{ $client->middle_name }}</td>
+                                            <td>{{ $client->last_name }} </td>
+                                            <td>{{ $client->email_address }}</td>
+                                            <td>{{ $client->phone_number }}</td>
+                                            <td>{{ $client->created_at }}</td>
+                                            <td>{{ $client->updated_at }}</td>
+                                            <td>{{ $client->region }}</td>
+                                            <td>
+                                                <a href="{{ url('reports/clients/' . $client->user_detail_id) }}"
+                                                   data-toggle="tooltip"
+                                                   title="Click here for more details"><i class="fa fa-binoculars"></i></a>
+                                            </td>
+                                        </tr>
 
-                                @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
 
-                    @else
+                        @else
 
-                        <h2 class="text-center">Empty</h2>
+                            <h2 class="text-center">Empty</h2>
 
-                    @endif
+                        @endif
 
-                </div>
-                <div role="tabpanel" class="tab-pane" id="reserved">
-
-
-                </div>
-                <div role="tabpanel" class="tab-pane" id="unreserved">
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="reserved">
 
 
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="unreserved">
+
+
+                    </div>
                 </div>
             </div>
         </div>
