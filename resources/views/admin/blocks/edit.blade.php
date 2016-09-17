436211a6714da25c63ba80777a222ddff2864156
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-09-17 16:42:18 +0300

    Clean blocks UI

--- resources/views/admin/blocks/edit.blade.php
+++ resources/views/admin/blocks/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin')
 
-@section('page_title' , ' - Admin')
+@section('page_title', 'Admin')
 
 @section('nav_bar')
 
@@ -16,24 +16,38 @@
 
 @section('main_content')
 
-    <h3>Edit {{ $block->name }}</h3>
+    <div class="row">
+        <div class="col-xs-12">
+            <h2>Block</h2>
+            <a href="{{ url('/admin/blocks') }}" class="btn btn-primary pull-right">View All</a>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">#</h3>
+                </div>
+                <div class="panel-body">
 
-    <div class="well">
+                    <h2>Edit {{ $block->name }}</h2>
 
-        @include('common.errors')
+                    @include('common.errors')
 
-        {!! Form::model($block, ['method' => 'PATCH', 'action' => ['Admin\BlockController@update', $block->block_id] , 'class' => 'form-horizontal']) !!}
+                    {!! Form::model($block, ['method' => 'PATCH', 'action' => ['Admin\BlockController@update', $block->block_id] , 'class' => 'form-horizontal']) !!}
 
-        @include('admin.blocks._form')
+                    @include('admin.blocks._form')
 
-        <div class="form-group">
-            <div class="col-sm-9 col-sm-offset-3">
-                <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Update</button>
+                    <div class="form-group">
+                        <div class="col-sm-9 col-sm-offset-3">
+                            <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Update</button>
+                        </div>
+                    </div>
+
+                    {!! Form::close() !!}
+                </div>
             </div>
         </div>
-
-        {!! Form::close() !!}
-
     </div>
-
 @endsection
