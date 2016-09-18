b36d96f4f0ee4f6f5f9eac254cacf86dd5860335
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-09-17 19:37:17 -0700

    Clean clients report UI

--- resources/views/reports/clients/index.blade.php
+++ resources/views/reports/clients/index.blade.php
@@ -1,6 +1,12 @@
-@extends('layouts.reports')
+@extends('layouts.admin')
 
-@section('page_title', 'Reports')
+@section('page_title', 'Admin')
+
+@section('nav_bar')
+
+    @include('admin.common.nav_bar')
+
+@endsection
 
 @section('side_bar')
 
@@ -8,99 +14,72 @@
 
 @endsection
 
-@section('content')
+@section('main_content')
 
     <div class="row">
-        <div class="col-sm-12">
-            <ol class="breadcrumb">
-                <li>
-                    <a href="{{ url('/') }}">CDA</a>
-                </li>
-                <li class="active">Clients</li>
-            </ol>
+        <div class="col-xs-12">
+            <h2>Clients</h2>
+            <a href="{{ url('/reports/clients/new') }}" class="btn btn-primary pull-right">New</a>
         </div>
     </div>
+    <br>
 
     <div class="row">
+        <div class="col-xs-12">
 
-        <div class="col-sm-12">
-            <div role="tabpanel">
-                <!-- Nav tabs -->
-                <ul class="nav nav-tabs" role="tablist">
-                    <li role="presentation" class="active">
-                        <a href="#all" aria-controls="all" role="tab" data-toggle="tab">All</a>
-                    </li>
-                    <li role="presentation">
-                        <a href="#reserved" aria-controls="reserved" role="tab" data-toggle="tab">Partially Registered</a>
-                    </li>
-                    <li role="presentation">
-                        <a href="#unreserved" aria-controls="unreserved" role="tab" data-toggle="tab">Fully Registered</a>
-                    </li>
-                </ul>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">#</h3>
+                </div>
+                <div class="panel-body">
+                    @if(count($clients) > 0)
 
-                <!-- Tab panes -->
-                <div class="tab-content">
-                    <div role="tabpanel" class="tab-pane active" id="all">
-
-                        @if(count($clients) > 0)
-
-                            <div class="table-responsive">
-                                <table class="table table-hover">
-                                    <thead>
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <thead>
+                                <tr>
+                                    <th>First Name</th>
+                                    <th>Middle Name</th>
+                                    <th>Last Name</th>
+                                    <th>Email Address</th>
+                                    <th>Phone Number</th>
+                                    <th>Registered</th>
+                                    <th>Last Login</th>
+                                    <th>Region</th>
+                                    <th><i class="fa fa-info-circle"></i></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($clients as $client)
                                     <tr>
-                                        <th>First Name</th>
-                                        <th>Middle Name</th>
-                                        <th>Last Name</th>
-                                        <th>Email Address</th>
-                                        <th>Phone Number</th>
-                                        <th>Registered</th>
-                                        <th>Last Login</th>
-                                        <th>Region</th>
-                                        <th><i class="fa fa-info-circle"></i></th>
+                                        <td>{{ $client->first_name }}</td>
+                                        <td>{{ $client->middle_name }}</td>
+                                        <td>{{ $client->last_name }} </td>
+                                        <td>{{ $client->email_address }}</td>
+                                        <td>{{ $client->phone_number }}</td>
+                                        <td>{{ $client->created_at }}</td>
+                                        <td>{{ $client->updated_at }}</td>
+                                        <td>{{ $client->region }}</td>
+                                        <td>
+                                            <a href="{{ url('reports/clients/' . $client->user_detail_id) }}"
+                                               data-toggle="tooltip"
+                                               title="Click here for more details"><i class="fa fa-binoculars"></i></a>
+                                        </td>
                                     </tr>
-                                    </thead>
-                                    <tbody>
-                                    @foreach($clients as $client)
-                                        <tr>
-                                            <td>{{ $client->first_name }}</td>
-                                            <td>{{ $client->middle_name }}</td>
-                                            <td>{{ $client->last_name }} </td>
-                                            <td>{{ $client->email_address }}</td>
-                                            <td>{{ $client->phone_number }}</td>
-                                            <td>{{ $client->created_at }}</td>
-                                            <td>{{ $client->updated_at }}</td>
-                                            <td>{{ $client->region }}</td>
-                                            <td>
-                                                <a href="{{ url('reports/clients/' . $client->user_detail_id) }}"
-                                                   data-toggle="tooltip"
-                                                   title="Click here for more details"><i class="fa fa-binoculars"></i></a>
-                                            </td>
-                                        </tr>
 
-                                    @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
 
-                        @else
+                    @else
 
-                            <h2 class="text-center">Empty</h2>
+                        <h2 class="text-center">Empty</h2>
 
-                        @endif
-
-                    </div>
-                    <div role="tabpanel" class="tab-pane" id="reserved">
-
-
-                    </div>
-                    <div role="tabpanel" class="tab-pane" id="unreserved">
-
-
-                    </div>
+                    @endif
                 </div>
             </div>
+
         </div>
-
     </div>
-
 @endsection
